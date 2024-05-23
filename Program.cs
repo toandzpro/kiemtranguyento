ca06df56cea390dd7c0e5f36cfd7055a9c3ceebf
toandzpro <kimthoai15620@gmail.com>
2024-05-24 05:26:08 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿public class baitap14
+{
+    public static void Main()
+    {
+        int number;
+        Console.WriteLine("Enter a number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        if (number < 2)
+            Console.WriteLine(number + " is not a prime");
+        else
+        {
+            int i = 2;
+            bool check = true;
+            while (i <= Math.Sqrt(number))
+            {
+                if (number % i == 0)
+                {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+                Console.WriteLine(number + " is a prime");
+            else
+                Console.WriteLine(number + " is not a prime");
+        }
+    }
+}
